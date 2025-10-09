9cbede9f5e27e32d72402dca19c3f71c86ea1658
Moisarm <moisarm03@gmail.com>
2025-10-08 23:12:23 -0400

    [Feature] (entity) : domain user entity

---
+++ Backend/src/domain/entities/user.entity.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
