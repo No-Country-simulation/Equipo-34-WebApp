13c02451fb94e12348b35a66d3029764e71333f8
Moisarm <moisarm03@gmail.com>
2025-10-09 17:07:11 -0400

    [Feature] (docker) : completed docker compose for setting up the postgres container

--- Backend/src/presentation/routes/health.route.ts
+++ Backend/src/presentation/routes/health.route.ts
@@ -8,7 +8,7 @@ const server_health_instance = new server_health();
 health_router.get("/", async (req: Request, res: Response) => {
   const response = await server_health_instance.server_health_controller();
 
-  res.status(response.status).json(response.message);
+  res.status(response.status).json(response);
 });
 
 export default health_router;
