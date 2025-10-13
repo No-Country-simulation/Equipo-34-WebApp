4e5a3208abf343a27277cef03a89c46041779205
Moisarm <moisarm03@gmail.com>
2025-10-13 03:03:19 -0400

    [Fix] (delete) : deleted some useless files

--- Backend/src/domain/repositories/patient/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Patient } from "../../entities/patient.entity";
-
-export interface patient_repository {
-  get_all_patients(): Promise<Patient[]>;
-  search_patient(id: string): Promise<Patient>;
-  create_patient(new_patient: Patient): Promise<Patient>;
-  update_patient(data: Patient): Promise<Patient>;
-  delete_patient(id: string): Promise<void>;
-}
--- Backend/src/domain/repositories/user/index.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from "../../entities/user.entity";
-
-export interface user_repository {
-  get_users(): Promise<User[]>;
-  search_user_by_ID(id: string): Promise<User | null>;
-  search_user_by_email(email: string): Promise<User | null>;
-  delete_user(id: string): Promise<null>;
-}
