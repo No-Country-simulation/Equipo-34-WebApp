12edcb04fd5232fa85eaa6ef7802e4623147ac6e
Moisarm <moisarm03@gmail.com>
2025-10-13 05:06:06 -0400

    [Feature] (entity) : permission and user entities

--- Backend/src/domain/entities/permission.entity.ts
+++ Backend/src/domain/entities/permission.entity.ts
@@ -2,7 +2,7 @@ import type { Role } from "./role.entity";
 
 export interface Permission {
   id: number;
-  permission_name: string;
+  name: string;
   description: string;
   roles?: Role[]; // Relación inversa
 }
--- Backend/src/domain/entities/user.entity.ts
+++ Backend/src/domain/entities/user.entity.ts
@@ -6,10 +6,10 @@ export interface User {
   last_name: string;
   password: string;
   phone: string;
-  emergency_contact: string | null;
-  two_factor_auth: Date | null;
+  emergency_contact?: string | null;
+  two_factor_auth?: Date | null;
   role_id: number;
   role?: Role; // Relación directa
   created_at: Date;
-  updated_at: Date;
+  updated_at?: Date;
 }
